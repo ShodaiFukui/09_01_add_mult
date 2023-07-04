bf69844684dec8e8051f82cfb67bd54eefc50b2f
Shodai Fukui <cmt14111@ict.nitech.ac.jp>
2023-07-04 04:51:11 +0000

    add main and dummy function

---
+++ test.py
@@ -0,0 +1,14 @@
+def min(a, b):
+    return 0
+
+def main():
+    
+    a = 1
+    b = 2
+    
+    print("a = \n" + a)
+    print("b = \n" + b)
+    print("最小値 : \n" + min(a, b))
+
+if __name__ == "__main__":
+    main()
