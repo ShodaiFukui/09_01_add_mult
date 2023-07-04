37b877346c26be64ee18e272442e913312cc41c1
Shodai Fukui <cmt14111@ict.nitech.ac.jp>
2023-07-04 05:17:54 +0000

    add test program

--- test.py
+++ test.py
@@ -2,12 +2,7 @@ def min(a, b):
     return 0
 
 def main():
-    a = 1
-    b = 2
-    
-    print("a = " + str(a))
-    print("b = " + str(b))
-    print("最小値 : " + str(min(a, b)))
+    return 0
 
 if __name__ == "__main__":
     main()
---
+++ test_test.py
@@ -0,0 +1,11 @@
+import unittest
+from test import min
+
+class TestMyTest(unittest.TestCase):
+    
+    def test_min1(self):
+        val = min(1, 2)
+        self.assertEqual(1, val)
+        
+if __name__ == "__main__":
+    unittest.main()
