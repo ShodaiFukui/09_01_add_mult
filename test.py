8e822be5e25a122867a9edf7422bf3a1b506642c
Shodai Fukui <cmt14111@ict.nitech.ac.jp>
2023-07-04 05:27:43 +0000

    fix function and make new test

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 def min(a, b):
+    if(a >= 0 and b >=0 and a < b):
+        return a
     return 0
 
 def main():
--- test_test.py
+++ test_test.py
@@ -6,6 +6,10 @@ class TestMyTest(unittest.TestCase):
     def test_min1(self):
         val = min(1, 2)
         self.assertEqual(1, val)
+    
+    def test_min1(self):
+        val = min(2, 1)
+        self.assertEqual(1, val)
         
 if __name__ == "__main__":
     unittest.main()
